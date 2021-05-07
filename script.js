fe2e0c7d98e9ad141a80228d51323f523b3f7310
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-05-07 01:04:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+alert('connnected')
+
+const person = {
+    firstName: ' Viggo',
+    lastName: 'Montreal',
+    fullName: (() =>{
+        return `${this.firstName},${this.lastName}`
+    })
+}
+
+console.log(fullName)
+
+function rolldie(numSide = 6){
+    return Math.floor(math.random() * numSides) + 1
+}
