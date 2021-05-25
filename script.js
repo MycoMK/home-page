573515f884de8382d4a73056183b963fddd6f6dd
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-05-24 23:26:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function rolldie(numSide = 6){
 }
 
 document.querySelector('h1').innerHTML = '<i> Listening , presenting, briefing and body language</i>'
+
+
+
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50]
+
+// getting the total of all prices
+const total = prices.reduce((total, price) => {
+return total + price
+})
+
+console.log(total)
